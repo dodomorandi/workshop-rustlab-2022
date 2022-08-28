c4f6b7ba709c9cb3599eb56678770b1c193ac2b1
Edoardo Morandi <morandidodo@gmail.com>
2022-08-28 16:52:15 +0200

    feat: impl final structure of main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
+use std::future::ready;
+
+use futures_util::{future, stream, TryStreamExt};
+use reqwest::Client;
+use workshop_rustlab_2022::database::{self, GeoPoint2d};
+
+use crate::my_stream::MyStream;
+
 mod my_stream;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use database::ServerField::*;
+
     tracing_subscriber::fmt::init();
-    todo!()
+
+    let stream = MyStream::<Vec<MyEntry>>::new(
+        [Name, GeoPoint2d, Numeromoderno].into_iter().collect(),
+        Some(8080),
+        Client::new(),
+    );
+
+    stream
+        .map_ok(|entries| stream::iter(entries.into_iter().map(Ok)))
+        .try_flatten()
+        .try_filter(|data| ready(data.name != "-"))
+        .try_for_each(|data| {
+            println!("{}: {:#?}", data.name, data.geo_point_2d);
+            future::ok::<_, my_stream::Error>(())
+        })
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct MyEntry {
+    pub geo_point_2d: GeoPoint2d,
+    pub name: String,
+    pub numeromoderno: String,
 }
--- src/my_stream.rs
+++ src/my_stream.rs
@@ -1,16 +1,26 @@
 use std::{
+    fmt,
     marker::PhantomData,
     pin::Pin,
     task::{Context, Poll},
 };
 
 use futures_util::Stream;
+use reqwest::Client;
+use workshop_rustlab_2022::database::{calc_query_cost, ServerField, ServerQuery};
 
 pub(crate) struct MyStream<T> {
+    query: ServerQuery,
+    port: Option<u16>,
+    client: Client,
+    query_cost: u16,
     _marker: PhantomData<fn() -> T>,
 }
 
-pub(crate) enum Error {}
+#[derive(Debug)]
+pub(crate) enum Error {
+    Reqwest(reqwest::Error),
+}
 
 impl<T> Stream for MyStream<T> {
     type Item = Result<T, Error>;
@@ -19,3 +29,36 @@ impl<T> Stream for MyStream<T> {
         todo!()
     }
 }
+
+impl<T> MyStream<T> {
+    pub(crate) fn new(
+        fields: std::collections::HashSet<ServerField>,
+        port: Option<u16>,
+        client: Client,
+    ) -> Self {
+        let query = ServerQuery {
+            fields,
+            ..Default::default()
+        };
+
+        let query_cost = calc_query_cost(&query);
+
+        Self {
+            query,
+            port,
+            client,
+            query_cost,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Reqwest(err) => write!(f, "Reqwest error: {err}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
